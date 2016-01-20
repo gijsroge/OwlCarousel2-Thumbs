ed81d9e1c5d271442bd519d6a2416d0d17a4b483
Gijs Rogé <rogegijs@gmail.com>
2016-01-20 16:35:38 +0100

    fixed version

--- dist/owl.carousel2.thumbs.js
+++ dist/owl.carousel2.thumbs.js
@@ -1,4 +1,4 @@
-/*! owl.carousel2.thumbs - v0.1.6 | (c) 2016 @gijsroge | MIT license | https://github.com/gijsroge/OwlCarousel2-Thumbs */
+/*! owl.carousel2.thumbs - v0.1.7 | (c) 2016 @gijsroge | MIT license | https://github.com/gijsroge/OwlCarousel2-Thumbs */
 /**
  * Thumbs Plugin
  * @version 2.0.0
